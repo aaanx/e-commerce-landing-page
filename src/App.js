46366634e9fcf1ef5ca9251abeae94d8cf655969
Anna <anna.walkowiakk@gmail.com>
2020-09-01 18:57:37 +0200

    ADD ArrivalsPage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import WelcomePage from "./components/WelcomePage/WelcomePage";
+import ArrivalsPage from "./components/ArrivalsPage/ArrivalsPage";
 
 function App() {
   return (
     <div className="page-wrapper">
       <WelcomePage />
+      <ArrivalsPage />
     </div>
   );
 }

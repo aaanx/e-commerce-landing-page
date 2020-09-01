fb2439c7751e3b42a2179385b38397d2453103b3
Anna <anna.walkowiakk@gmail.com>
2020-09-01 18:58:49 +0200

    ADD json data as state

--- src/components/ArrivalsItems/ArrivalsItems.js
+++ src/components/ArrivalsItems/ArrivalsItems.js
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ArrivalsItem from "./../ArrivalsItem/ArrivalsItem";
 import "./ArrivalsItems.css";
 
-var data = require("../../products.json");
-console.log(data);
+function ArrivalsItems(props) {
+  useEffect(() => {
+    if (props.bags.length != 0) {
+      console.log(props.bags);
+    }
+  }, [props]);
 
-class ArrivalsItems extends React.Component {
-  render() {
-    return (
-      <div className="arrivals-items">
-        <ArrivalsItem id="1" />
-        <ArrivalsItem id="2" />
-        <ArrivalsItem id="3" />
-        <ArrivalsItem id="4" />
-        <ArrivalsItem id="5" />
-        <ArrivalsItem id="6" />
-      </div>
-    );
-  }
+  return (
+    <div className="arrivals-items">
+      <ArrivalsItem id="1" />
+      <ArrivalsItem id="2" />
+      <ArrivalsItem id="3" />
+      <ArrivalsItem id="4" />
+      <ArrivalsItem id="5" />
+      <ArrivalsItem id="6" />
+    </div>
+  );
 }
 
 export default ArrivalsItems;
--- src/components/ArrivalsPage/ArrivalsPage.js
+++ src/components/ArrivalsPage/ArrivalsPage.js
@@ -5,11 +5,37 @@ import AddToCartButton from "./../AddToCartButton/AddToCartButton";
 import "./ArrivalsPage.css";
 
 class ArrivalsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      products: {
+        bags: [],
+        watches: [],
+      },
+    };
+  }
+
+  componentDidMount() {
+    var data = require("../../products.json");
+    this.setState({
+      isLoaded: true,
+      products: {
+        bags: [data.bags],
+        watches: [data.watches],
+      },
+    });
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="arrivals-page">
         <ArrivalsHeader />
-        <ArrivalsItems />
+        <ArrivalsItems bags={this.state.products.bags} />
         <AddToCartButton />
       </div>
     );

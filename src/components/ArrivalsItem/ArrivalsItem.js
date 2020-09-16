59fe7abf12b8f6a5de6e047514ad788bc40cb3b1
Anna <anna.walkowiakk@gmail.com>
2020-09-16 21:00:01 +0200

    MODIFY component type and use props

--- src/components/ArrivalsItem/ArrivalsItem.js
+++ src/components/ArrivalsItem/ArrivalsItem.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "./ArrivalsItem.css";
 
-class ArrivalsItem extends React.Component {
-  render() {
-    return (
-      <div className="arrivals-item">
-        <img src="#" />
-        <p>Black Genuine Leather Bag</p>
-        <p>$ 23.20</p>
-      </div>
-    );
-  }
+function ArrivalsItem(props) {
+  return (
+    <div className="arrivals-item">
+      <img src={props.img} className="arrivals-item-img" />
+      <p className="arrivals-item-name">{props.name} </p>
+      <p className="arrivals-item-price">{props.price} </p>
+    </div>
+  );
 }
 
 export default ArrivalsItem;

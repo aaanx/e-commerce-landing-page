e89796bd5c926d26aeceea5dc49f5b38e1212eb8
Anna <anna.walkowiakk@gmail.com>
2020-09-17 12:45:29 +0200

    ADD container with imported icons

--- src/components/ArrivalsItem/ArrivalsItem.js
+++ src/components/ArrivalsItem/ArrivalsItem.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./ArrivalsItem.css";
+import FavoriteBorderIcon from "../../images/icons/favorite_border.svg";
+import MessageIcon from "../../images/icons/message.svg";
 
 function ArrivalsItem(props) {
   return (
     <div className="arrivals-item">
+      <div className="arrivals-item-icons">
+        <span className="arrivals-item-favorite">
+          <img
+            src={FavoriteBorderIcon}
+            className="arrivals-item-favorite-icon"
+          />
+          <span className="arrivals-item-favorite-num">253</span>
+        </span>
+        <span className="arrivals-item-message">
+          <img src={MessageIcon} className="arrivals-item-message-icon" />
+          <span className="arrivals-item-message-num">120</span>
+        </span>
+      </div>
       <img src={props.img} className="arrivals-item-img" />
       <p className="arrivals-item-name">{props.name} </p>
       <p className="arrivals-item-price">{props.price} </p>
+      <button className="add-btn">Add to Cart</button>
     </div>
   );
 }

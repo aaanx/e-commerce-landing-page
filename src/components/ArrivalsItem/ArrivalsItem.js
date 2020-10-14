9b8fa39057d626c53d85db68878c1ea9d4ad9bb2
Anna <anna.walkowiakk@gmail.com>
2020-10-14 17:41:34 +0200

    ADD onHover effect on Arrivals Item

--- src/components/ArrivalsItem/ArrivalsItem.js
+++ src/components/ArrivalsItem/ArrivalsItem.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ArrivalsItem.css";
 import FavoriteBorderIcon from "../../images/icons/favorite_border.svg";
 import MessageIcon from "../../images/icons/message.svg";
 
 function ArrivalsItem(props) {
+  const [iconsDisplay, setIconsDisplay] = useState("none");
+  const [addBtnDisplay, setAddBtnDisplay] = useState("none");
+  const [nameFontColor, setNameFontColor] = useState("#777");
+  const [priceFontColor, setPriceFontColor] = useState("#333");
+
+  function handleArrivalItemMouseEnter() {
+    setIconsDisplay("flex");
+    setAddBtnDisplay("block");
+    setNameFontColor("#333");
+    setPriceFontColor("#8c84f9");
+  }
+
+  function handleArrivalItemMouseLeave() {
+    setIconsDisplay("none");
+    setAddBtnDisplay("none");
+    setNameFontColor("#777");
+    setPriceFontColor("#333");
+  }
+
   return (
-    <div className="arrivals-item">
-      <div className="arrivals-item-icons">
+    <div
+      className="arrivals-item"
+      onMouseEnter={handleArrivalItemMouseEnter}
+      onMouseLeave={handleArrivalItemMouseLeave}
+    >
+      <div className="arrivals-item-icons" style={{ display: iconsDisplay }}>
         <span className="arrivals-item-favorite">
           <img
             src={FavoriteBorderIcon}
@@ -20,9 +43,15 @@ function ArrivalsItem(props) {
         </span>
       </div>
       <img src={props.img} className="arrivals-item-img" />
-      <p className="arrivals-item-name">{props.name} </p>
-      <p className="arrivals-item-price">{props.price} </p>
-      <button className="add-btn">Add to Cart</button>
+      <p className="arrivals-item-name" style={{ color: nameFontColor }}>
+        {props.name}{" "}
+      </p>
+      <p className="arrivals-item-price" style={{ color: priceFontColor }}>
+        {props.price}{" "}
+      </p>
+      <button className="add-btn" style={{ display: addBtnDisplay }}>
+        Add to Cart
+      </button>
     </div>
   );
 }

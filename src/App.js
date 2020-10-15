942adf6e5c407c8bbf6d40ef7ecdb723bee2897d
Anna <anna.walkowiakk@gmail.com>
2020-10-15 12:16:44 +0200

    MODIFY data upload from json

--- src/App.js
+++ src/App.js
@@ -3,15 +3,46 @@ import "./App.css";
 import WelcomePage from "./components/WelcomePage/WelcomePage";
 import ArrivalsPage from "./components/ArrivalsPage/ArrivalsPage";
 import BrandsPage from "./components/BrandsPage/BrandsPage";
+import ShowcasePage from "./components/ShowcasePage/ShowcasePage";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      products: {
+        bags: [],
+        watches: [],
+      },
+    };
+  }
+
+  componentDidMount() {
+    var data = require("./products.json");
+    this.setState({
+      isLoaded: true,
+      products: {
+        bags: data.bags,
+        watches: data.watches,
+      },
+    });
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.products.bags);
+  }
+
+render(){
   return (
     <div className="page-wrapper">
       <WelcomePage />
-      <ArrivalsPage />
+      <ArrivalsPage bags={this.state.products.bags} />
       <BrandsPage />
+      {/* <ShowcasePage /> */}
     </div>
-  );
+  );  
+}
+
 }
 
 export default App;
--- src/components/ArrivalsPage/ArrivalsPage.js
+++ src/components/ArrivalsPage/ArrivalsPage.js
@@ -3,41 +3,15 @@ import ArrivalsHeader from "./../ArrivalsHeader/ArrivalsHeader";
 import ArrivalsItems from "./../ArrivalsItems/ArrivalsItems";
 import "./ArrivalsPage.css";
 
-class ArrivalsPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoaded: false,
-      products: {
-        bags: [],
-        watches: [],
-      },
-    };
-  }
-
-  componentDidMount() {
-    var data = require("../../products.json");
-    this.setState({
-      isLoaded: true,
-      products: {
-        bags: data.bags,
-        watches: data.watches,
-      },
-    });
-  }
-
-  componentDidUpdate() {
-    console.log(this.state.products.bags);
-  }
-
-  render() {
+function ArrivalsPage(props) {
+  
     return (
       <div className="arrivals-page">
         <ArrivalsHeader />
-        <ArrivalsItems bags={this.state.products.bags} />
+        <ArrivalsItems bags={props.bags} />
       </div>
     );
-  }
+
 }
 
 export default ArrivalsPage;

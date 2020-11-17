1e4ae003f7d31bab69ee8d039e9e1fe0d9640a3c
Anna <anna.walkowiakk@gmail.com>
2020-11-17 17:54:28 +0100

    Updates

---
+++ precache-manifest.105ec138997f6903cd746a1694c081e7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1794c924b1d35dba97711f8df0939782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27c9ed2f6bf3bcd842a3",
+    "url": "/static/css/main.ff3dda52.chunk.css"
+  },
+  {
+    "revision": "694dacb538b493ca494b",
+    "url": "/static/js/2.789fecd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.789fecd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c9ed2f6bf3bcd842a3",
+    "url": "/static/js/main.3869503b.chunk.js"
+  },
+  {
+    "revision": "4d01e2888fc83a738f71",
+    "url": "/static/js/runtime-main.e04fa04f.js"
+  },
+  {
+    "revision": "ab2eb5904a224fc6c2c175c9a550d623",
+    "url": "/static/media/arrow_down.ab2eb590.svg"
+  },
+  {
+    "revision": "80e0e702cf79053c13842328d282d200",
+    "url": "/static/media/arrow_right.80e0e702.svg"
+  },
+  {
+    "revision": "52b5455a0eff84126daf926f84cf5b33",
+    "url": "/static/media/facebook-icon-gradient.52b5455a.svg"
+  },
+  {
+    "revision": "c7ecf461fac91c45ba47dd76f55150fd",
+    "url": "/static/media/favorite_border.c7ecf461.svg"
+  },
+  {
+    "revision": "b3750201d4e4e32867911cfa30ec31b0",
+    "url": "/static/media/google-plus-icon-gradient.b3750201.svg"
+  },
+  {
+    "revision": "e0f8e794a05eb377919555baa7252144",
+    "url": "/static/media/instagram-icon-gradient.e0f8e794.svg"
+  },
+  {
+    "revision": "76dc8473cbc6959d586fd6fb81580e8d",
+    "url": "/static/media/iphone.76dc8473.png"
+  },
+  {
+    "revision": "759cf5c9195a7ba83dfe7b47cd809ffe",
+    "url": "/static/media/message.759cf5c9.svg"
+  },
+  {
+    "revision": "19142cc51b8c074217df3d7499267bf0",
+    "url": "/static/media/search.19142cc5.svg"
+  },
+  {
+    "revision": "5236f921b41378442d7184f82fb0f5ba",
+    "url": "/static/media/twitter-icon-gradient.5236f921.svg"
+  }
+]);
